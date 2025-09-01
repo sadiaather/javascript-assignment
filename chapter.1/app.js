c7f26b0670621d83ab648d1099a18ffb191a3247
sadiaather <ishmalsiddiqui59@gmail.com>
2025-09-01 19:56:08 +0500

    work

---
+++ chapter.1/app.js
@@ -0,0 +1,19 @@
+// question no 1 
+// alert("welcome to my website")
+
+// question no.2
+// alert("Error! please enter a vaild passward")
+
+// question no.3
+// alert("wellcome to js land..\n happy codding")
+
+// question no .4
+// alert("welcome to js land")
+// alert( "happy coading ")
+
+// question no .5
+// alert("hello.. I can run JS through my web browser's console")
+
+// question no .6
+// alert("this is my first javascript pop practice")
+
