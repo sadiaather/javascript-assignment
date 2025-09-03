0c54dde85f417b6bae0038be42c379ca483f58df
sadiaather <ishmalsiddiqui59@gmail.com>
2025-09-04 00:52:48 +0500

    done

--- chapter.2/app.js
+++ chapter.2/app.js
@@ -1,5 +1,47 @@
 // question no.1
-var userName = ("sadia")
-console.log("userName")
+var userName = "sadia"
+console.log(userName)
 
-Document.write("sadia")
+// question no.2
+var myName="sadia ather"
+console.log(myName)
+fullName="sadia ather"
+console.log(myName)
+
+// question no.3
+// a
+var title = "message"
+console.log(title)
+// b
+title = "hello world"
+console.log(title)
+// c
+alert("hello world")
+
+// qustion no.4
+var studentName = "jhone doe"
+alert(studentName)
+var studentage = "15 years old"
+alert(studentage)
+var studentcourse = "certified mobile application devlopment"
+alert( studentcourse)
+
+// question n0.5
+var name ="PIZZA"
+alert( name)
+alert("PIZZA\n PIZZ \n PIZ \n PI \n P")
+
+// question no.6
+var email = "my email address is @ gmil.com "
+alert(email)
+
+// question n0.7
+var book = "I am trying to learn from the book A smarter \n way to learn javascript "
+alert(book)
+
+// question no.8
+document.write("yah!I can write HTML content through JavaScript" )
+
+// questionno.9
+var end = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(end)
