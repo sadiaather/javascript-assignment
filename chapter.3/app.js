2bdf781033ed0e643bfd1a8fa1c0940252a32dc2
sadiaather <ishmalsiddiqui59@gmail.com>
2025-09-04 20:39:59 +0500

    done

---
+++ chapter.3/app.js
@@ -0,0 +1,20 @@
+// question no.1
+let age
+age=15
+alert("I am "+ age + " years old" )
+
+// question no.2
+let number = 14
+alert("You have visited this site "+ number +" times")
+
+// quetion no.3
+let birthYear = 1990
+document.write("My birth year is " + birthYear)
+
+
+// question no.4
+let store = "IU fabric clothing store"
+let visitorName = "sadia"
+let product = "t-shirts"
+let quntity = 5
+document.write(  visitorName + " ordered " + quntity + " " + product + " on " + store )
