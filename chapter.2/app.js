8e04ababa27792ec49a13d59f901ad27b3ee39d9
sadiaather <ishmalsiddiqui59@gmail.com>
2025-09-04 19:01:12 +0500

    changes

--- chapter.2/app.js
+++ chapter.2/app.js
@@ -1,43 +1,39 @@
 // question no.1
-var userName = "sadia"
-console.log(userName)
+var userName 
 
 // question no.2
 var myName="sadia ather"
 console.log(myName)
-fullName="sadia ather"
-console.log(myName)
+
 
 // question no.3
 // a
 var title = "message"
-console.log(title)
 // b
 title = "hello world"
-console.log(title)
 // c
-alert("hello world")
+alert(title)
 
 // qustion no.4
 var studentName = "jhone doe"
 alert(studentName)
-var studentage = "15 years old"
-alert(studentage)
-var studentcourse = "certified mobile application devlopment"
-alert( studentcourse)
+var studentAge = "15"
+alert(studentAge + " years old")
+var studentCourse = "certified mobile application devlopment"
+alert( studentCourse)
 
 // question n0.5
-var name ="PIZZA"
+var name ="PIZZA \n PIZZ \n PIZ \n PI \n P"
 alert( name)
-alert("PIZZA\n PIZZ \n PIZ \n PI \n P")
+
 
 // question no.6
-var email = "my email address is @ gmil.com "
-alert(email)
+var email = " @ gmil.com "
+alert("my email address is"+ email)
 
 // question n0.7
-var book = "I am trying to learn from the book A smarter \n way to learn javascript "
-alert(book)
+var book = " A smarter \n way to learn javascript "
+alert("I am trying to learn from the book"+ book)
 
 // question no.8
 document.write("yah!I can write HTML content through JavaScript" )
