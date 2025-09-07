780963bf21313c864d1b2a84bb18ae5b0dbe4d6e
sadiaather <ishmalsiddiqui59@gmail.com>
2025-09-08 01:10:28 +0500

    done

---
+++ chapter.4/app.js
@@ -0,0 +1,32 @@
+// question no.1
+let name , age , course
+
+// question no.2
+
+// legal variables
+// 1. letter: a to z 
+// 2. underscores : user_name
+// 3. start $ sgin :$price
+// 4. name/camel case : userName
+// 5.number after letter : user1
+
+// illegal variables
+// 1. start with number :let 123 name
+// 2. use keyword :let
+// 3. use reserved key :let function key
+// 4. use hyphen : let ;
+// 5. speical character : let  @gmail.com
+
+// question no.3
+// a
+ document.write("<h1>Rules for naming JS variables</h1>"+"<br>")
+//  b
+ document.write("Variable names can only contain<b>$</b>, <b>_</b>, <b>number</b>  and<b>  letter</b> For example $my_1stVariable "+"<br>" )
+//  c
+document.write("Variables must begin with a <b>letter</b>, <b>$</b> or <b> _ </b> For example $name, _name or name"+"<br>")
+// d
+document.write("Variable names are case <b>sensitive</b> "+"<br>")
+// e
+document.write("Variable names should not be JS <b>keyword</b>"+"<br>")
+
+
