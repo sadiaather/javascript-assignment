ff2e1900bd62ebcee3ecfa4fdd63770ab1e77032
sadiaather <ishmalsiddiqui59@gmail.com>
2025-09-11 00:33:45 +0500

    done

---
+++ chapter.5/app.js
@@ -0,0 +1,52 @@
+// question no.1
+let num = 3
+let num2 = 5
+let sum = 8
+document.write(`sum of ${num} and ${num2} is ${sum} <br>`)
+
+// question no.2
+let sub = 2
+document.write(`subtract of ${num2} and ${num} is ${sub} <br>`)
+let multi = 15
+document.write(`multiplication of ${num2} and ${num} is ${multi} <br>`)
+let num3 = 15
+document.write(`division of ${num3} and ${num} is ${num2} <br>`)
+let first = 20
+let secound = 10
+let ans = 0
+document.write(`modulus of ${first} and ${secound} is ${ans} <br> <br> `)
+
+//   question no.3
+// a
+let number
+//   b
+document.write(`Value after variable declaeration is ${number} <br>`)
+//   c
+number = 5
+//   d
+document.write(`Initial value is ${number} <br>`)
+//  e
+number++
+number = number
+document.write(`Value after increment  is ${number} <br>`)
+//   f
+let number2 = 7
+//   g
+number + number2
+number = 13
+document.write(`Value after addition is ${number} <br>`)
+//  h
+number--
+document.write(`Value after decrement is ${number} <br>`)
+// i
+number / 3
+number = 4
+remainder = 0
+document.write(`The remainder is ${remainder} <br> <br>`)
+
+//    question no.4
+let ticket = 600
+let ticketBuy = 5
+ticket * ticketBuy
+document.write(`Total cost to buy ${ticketBuy} to a moive is ${ticket * ticketBuy} PRK <br> <br>`)
+
