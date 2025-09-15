5ddf5ca1681698fcb340058fe718d10434336801
sadiaather <ishmalsiddiqui59@gmail.com>
2025-09-15 17:26:23 +0500

    done

--- chapter.5/app.js
+++ chapter.5/app.js
@@ -50,3 +50,123 @@ let ticketBuy = 5
 ticket * ticketBuy
 document.write(`Total cost to buy ${ticketBuy} to a moive is ${ticket * ticketBuy} PRK <br> <br>`)
 
+// question no.5
+let no = 4
+let no2 = 1
+document.write(`${no} x ${no2} = ${no*no2}`)
+document.write("<br/>")
+no2++
+document.write(`${no} x ${no2} = ${no*no2}`)
+document.write("<br/>")
+no2++
+document.write(`${no} x ${no2} = ${no*no2}`)
+document.write("<br/>")
+no2++
+document.write(`${no} x ${no2} = ${no*no2}`)
+document.write("<br/>")
+no2++
+document.write(`${no} x ${no2} = ${no*no2}`)
+document.write("<br/>")
+no2++
+document.write(`${no} x ${no2} = ${no*no2}`)
+document.write("<br/>")
+no2++
+document.write(`${no} x ${no2} = ${no*no2}`)
+document.write("<br/>")
+no2++
+document.write(`${no} x ${no2} = ${no*no2}`)
+document.write("<br/>")
+no2++
+document.write(`${no} x ${no2} = ${no*no2}`)
+document.write("<br/>")
+no2++
+document.write(`${no} x ${no2} = ${no*no2}`)
+document.write("<br/>")
+no2++
+document.write("<br/>")
+document.write("<br/>")
+
+// question no.6
+let Celsius = 25
+let Fahrenheit = (25*9/5)+32
+document.write(`${Celsius}C is ${Fahrenheit}F`)
+document.write(`<br/>`)
+let F = 70
+let C = (70-32)*5/9
+document.write(`${F} is ${C}`)
+document.write(`<br/>`)
+document.write(`<br/>`)
+
+// question no.7
+let item1 = 650
+let item2 = 100
+let quntity1 = 3
+let quntity2 = 7
+let shipping = 100
+document.write(`<h1> Shopping cart </h1>`)
+document.write(`price of item 1 is ${item1} <br>`)
+document.write(`quntity of item 1 is ${quntity1} <br> `)
+document.write(`price of item 2 is ${item2} <br>`)
+document.write(`quntity of item 2 is ${quntity2} <br> `)
+let total = 650*3+100*7+100
+document.write(`total cost of your order is ${total} <br> `)
+document.write(`<br/>`)
+
+// question no.8
+let tmarks = 980
+let omarks = 804
+let percentage = 804/980*100
+document.write(`<h1> Mark sheet </h1>`)
+document.write(`total marks : ${tmarks} <br/>`)
+document.write(`mark obtained : ${omarks} <br/>`)
+document.write(`percentage : ${percentage} <br/>`)
+document.write(`<br/>`)
+
+// question no.9
+ dollar = 104.80
+ riyal = 28
+dollar =  104.80*10
+riyal = 25*28
+document.write(`<h1> Currency in PRK </h1>`)
+document.write(`total currency in PRK : ${dollar + riyal}`)
+
+// question no.10
+let x = 4
+x = 4;
+let y = ((5+4)*10)/2
+console.log(y)
+document.write(`<br/>`)
+
+// question no.11
+let year = 2025
+let byear = 2008
+let age = 2025-2008
+document.write(`<h1> Age calculator </h1>`)
+document.write(`current year : ${year} <br/>`)
+document.write(`birth year : ${byear} <br/>`)
+document.write(`your age is : ${age} <br/>`)
+document.write(`<br/>`)
+
+// question no.12
+let radius = 20
+// (Hint : Circumference of a circle = 2 π r , π = 3.142)
+let c = 2*3.142*20
+document.write(`<h1> The geometrizer </h1>`)
+document.write(`radius of circle is : ${radius} <br/>`)
+document.write(`the circumference is : ${c} <br/>`)
+// (Hint : Area of a circle = π r2, π = 3.142)
+let area = 3.142*(20*20)
+document.write(`the area is : ${area}`)
+
+// question no.13
+let snacks = "lays"
+let old  = 15
+let maxAge = 65
+let amount = 3 
+let result = (65-15)*3
+document.write(`<h1> Life time supply calculator </h1>`)
+document.write(`favourite snacks : ${snacks} <br/>`)
+document.write(`current age : ${old} <br/>`)
+document.write(`estimated maxium age : ${maxAge} <br/>`)
+document.write(`amount of snacks pr day : ${amount} <br/>`)
+document.write(`you will need ${result } lays to last you until the ripe old age of ${maxAge}`)
